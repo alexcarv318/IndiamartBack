ef0101980a939ef026d8212e447337b5073f77e8
alexcarv318 <alexilchenko2005@gmail.com>
2024-10-04 19:15:10 +0300

    Edited the paths of amex. There was mistake

--- routers/amex.py
+++ routers/amex.py
@@ -5,28 +5,28 @@ from repositories.amex_repo import AmexRepository
 
 router = APIRouter(prefix="/amex", tags=["amex"])
 
-@router.get("/contactors/cities")
+@router.get("/contractors/cities")
 def get_cities(
     amex_repo: AmexRepository = Depends(get_amex_repo)
 ):
     cities = amex_repo.get_cities()
     return {"contactor_cities": cities}
 
-@router.get("/contactors/states")
+@router.get("/contractors/states")
 def get_states(
     amex_repo: AmexRepository = Depends(get_amex_repo)
 ):
     states = amex_repo.get_states()
     return {"contactor_states": states}
 
-@router.get("/contactors/categories")
+@router.get("/contractors/categories")
 def get_categories(
     amex_repo: AmexRepository = Depends(get_amex_repo)
 ):
     categories = amex_repo.get_categories()
     return {"contactor_categories": categories}
 
-@router.get("/contactors/filter")
+@router.get("/contractors/filter")
 def filter_contractors(
     amex_repo: AmexRepository = Depends(get_amex_repo),
     name: str | None = None,
